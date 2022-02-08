2e296a699f20366da90fcee7e68145de506d4b4d
Gillian Sanchez <xGillYan@gmail.com>
2022-02-08 17:15:15 -0500

    Generate a random Date of Birth function

--- README.md
+++ README.md
@@ -1,18 +1,7 @@
-## Getting Started
+## Goal
 
-Welcome to the VS Code Java world. Here is a guideline to help you get started to write Java code in Visual Studio Code.
+This project is focused around simulating the birthday paradox.
 
-## Folder Structure
+> The birthday paradox, also known as the birthday problem, states that in a random group of 23 people, there is about a 50 percent chance that two people have the same birthday.
 
-The workspace contains two folders by default, where:
-
-- `src`: the folder to maintain sources
-- `lib`: the folder to maintain dependencies
-
-Meanwhile, the compiled output files will be generated in the `bin` folder by default.
-
-> If you want to customize the folder structure, open `.vscode/settings.json` and update the related settings there.
-
-## Dependency Management
-
-The `JAVA PROJECTS` view allows you to manage your dependencies. More details can be found [here](https://github.com/microsoft/vscode-java-dependency#manage-dependencies).
+I found this to be a really intriguing concept, so I decided to start this small project.
--- src/App.java
+++ src/App.java
@@ -1,5 +1,42 @@
+import java.util.GregorianCalendar;
+
 public class App {
     public static void main(String[] args) throws Exception {
-        System.out.println("Hello, World!");
+        // declare variables
+        GregorianCalendar cal;
+        
+        cal = getRandDOB();
+
+        System.out.println(cal.get(GregorianCalendar.YEAR) + "-" + (cal.get(GregorianCalendar.MONTH) + 1) + "-" + cal.get(GregorianCalendar.DAY_OF_MONTH));
+    }
+
+    /**
+     * Generate and return a random date of birth between 1922 and 2022
+     * @return  A GregorianCalendar variable with a randomly generated date
+     */
+    private static GregorianCalendar getRandDOB() {
+        // declare variables
+        GregorianCalendar cal;
+        int iYear, iDay;
+
+        // initialize variables
+        cal = new GregorianCalendar();
+        iYear = getRand(1922, 2022);
+        cal.set(GregorianCalendar.YEAR, iYear);
+        iDay = getRand(1, cal.getActualMaximum(GregorianCalendar.DAY_OF_YEAR));
+        cal.set(GregorianCalendar.DAY_OF_YEAR, iDay);
+
+        return cal;
+    }
+
+
+    /**
+     * Returns a randomly generated number between min and max.
+     * @param min   The lowest possible number to return
+     * @param max   The highest possible number to return.
+     * @return      A number between min and max.
+     */
+    private static int getRand(int min, int max) {
+        return (int) ((Math.random() * (max - min)) + min);
     }
 }
